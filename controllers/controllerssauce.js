db69c0fc7e0670fec94b45453ef18063d343f511
NiPengNeee-Wom <zaphod1er@gmail.com>
2021-10-29 09:53:29 +0200

    Update routes sauces

--- controllers/controllerssauce.js
+++ controllers/controllerssauce.js
@@ -18,6 +18,12 @@ exports.modifySauce = (req, res, next) => {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`);
+        })
+        .catch(error => res.status(500).json({ error }));
     Sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
